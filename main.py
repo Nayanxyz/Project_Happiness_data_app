86c5a1f5fccccf8e72b2c4a93d19b1a61c130016
NayanPython <choudharyji527@gmail.com>
2026-03-08 12:32:24 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,34 @@
+import streamlit as st
+import plotly.express as px
+import pandas as pd
+
+
+st.title("In search for Happiness")
+
+select_box = st.selectbox("Select the data for the X-axis", ("GDP", "Happiness", "Generosity"))
+
+select__box = st.selectbox("Select the data for the Y-axis", ("GDP", "Happiness", "Generosity"))
+
+st.subheader(f" {select_box} and {select__box}")
+
+df = pd.read_csv("happy.csv")
+
+match select_box:
+    case "GDP":
+        x_array = df["gdp"]
+    case "Happiness":
+        x_array = df["happiness"]
+    case "Generosity":
+        x_array = df["generosity"]
+match select__box:
+    case "GDP":
+        y_array = df["gdp"]
+
+    case "Happiness":
+        y_array = df["happiness"]
+
+    case "Generosity":
+        y_array = df["generosity"]
+
+figure = px.scatter(x=x_array, y=y_array , labels={"x": select_box, "y": select__box})
+st.plotly_chart(figure)
